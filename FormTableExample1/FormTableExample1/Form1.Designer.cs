3a717fe0bc8b9821dec66a3536b84c4ca9ed61cb
santovious <44790141+santovious@users.noreply.github.com>
2019-05-16 13:52:41 -0700

    Add files via upload

---
+++ FormTableExample1/FormTableExample1/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace FormTableExample1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Table1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // Table1
+            // 
+            this.Table1.ColumnCount = 4;
+            this.Table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.Table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.Table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.Table1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.Table1.Location = new System.Drawing.Point(13, 13);
+            this.Table1.Name = "Table1";
+            this.Table1.RowCount = 1;
+            this.Table1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.Table1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.Table1.Size = new System.Drawing.Size(200, 100);
+            this.Table1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Table1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel Table1;
+    }
+}
+
---
+++ FormTableExample1/FormTableExample1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormTableExample1
+{
+    public partial class Form1 : Form
+    {
+        // Here we have some class level components for the form
+        public List<Label> rowLabels0 = new List<Label>();
+        public List<Label> rowLabels1 = new List<Label>();
+        public List<Label> rowLabels2 = new List<Label>();
+        public List<FlowLayoutPanel> countPanel = new List<FlowLayoutPanel>();
+        List<int> maleCount = new List<int>();
+        List<int> femaleCount = new List<int>();
+        public Form1()
+        {
+            /*
+             * First we want to set some rules for the form
+             */
+            AutoSize = true;
+            MaximumSize = new Size(800, 500);
+            AutoScroll = true;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // make the form !visible for now
+            this.Visible = false;
+            // set some rules on the table
+            Table1.AutoSize = true;
+            Table1.GrowStyle = TableLayoutPanelGrowStyle.AddRows;
+            Table1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            // some values to put in the labels
+            string[] somecolors = new string[] { "RED", "BLUE", "ORANGE", "PURPLE"};
+            string[] someshapes = new string[] { "Square", "Circle", "Triangle", "Rectangle" };
+            string[] somejunk = new string[] { "Crayon", "Pumpkin", "Robot", "Shrimp", "Onion" };
+            string[] dumbstuff = new string[] { "Your", "New", "Anime", "Count" };
+
+            int rowcount = 1;
+            int labelCount = 0;
+            for (int i = 0; i < dumbstuff.Length; i++)
+            {// set the header labels in row 0 column i
+                Table1.Controls.Add((new Label
+                {
+                    Text = dumbstuff[i],
+                    BorderStyle = BorderStyle.Fixed3D
+                }), i, 0);
+            }
+            for (int j = 0; j < somecolors.Length; j++)
+            {// loop holds the count for column 0 row rowcount
+                for (int q = 0; q < someshapes.Length; q++)
+                {// loop holds the count for column 1 row rowcount
+                    for (int z = 0; z < somejunk.Length; z++)
+                    {// loop holds the count for column 2 row rowcount
+
+                        // dynamically adding labels to the list
+                        rowLabels0.Add(new Label {
+                            Text = somecolors[j],
+                            BorderStyle = BorderStyle.FixedSingle
+                        } );
+                        rowLabels1.Add(new Label
+                        {
+                            Text = someshapes[q],
+                            BorderStyle = BorderStyle.FixedSingle
+                        });
+                        rowLabels2.Add(new Label
+                        {
+                            Text = somejunk[z],
+                            BorderStyle = BorderStyle.FixedSingle
+                        });
+                        // here the table requires a call to the controls with an Add.
+                        // This takes 3 arguments: the item, the column, the row.
+                        Table1.Controls.Add(rowLabels0[labelCount], 0, rowcount);
+                        Table1.Controls.Add(rowLabels1[labelCount], 1, rowcount);
+                        Table1.Controls.Add(rowLabels2[labelCount], 2, rowcount);
+                        // row count allows for the number of rows to continually grow.
+                        rowcount++;
+                        // holds the index for each combination of labels being made.
+                        labelCount++;
+                    }
+                }
+            }
+            
+        }
+    }
+}
